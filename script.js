63f3f314d452b9f12ef406094a10011691c38a86
malika_G17 <120101174+malika08@users.noreply.github.com>
2023-01-05 03:09:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let js = 'amazing';
+    
+
+      console.log('Malika');
+      console.log(23);
+      console.log(20+2-5+20);
+
+      /*var job =' mmm';
+      job =' nnnn'*/
+
+      let x = 10+5;
+      x += 20;
+      
+      console.log(x);
